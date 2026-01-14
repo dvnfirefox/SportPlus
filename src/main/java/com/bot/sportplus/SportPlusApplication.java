380b96ccea473046f547bb247aaa6c5390aaf0b7
dvnfirefox <dvnfirefox123@gmail.com>
2026-01-13 20:20:30 -0500

    first commit

---
+++ src/main/java/com/bot/sportplus/SportPlusApplication.java
@@ -0,0 +1,13 @@
+package com.bot.sportplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/sportplus/SportPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.sportplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
